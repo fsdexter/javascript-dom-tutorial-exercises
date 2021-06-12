ffb44d80089f54841ff747476ea5531cb0fc1195
fsdexter <fsdexter@gmail.com>
2021-06-12 17:33:54 +0000

    solution and doubts further analysis needed

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -1,8 +1,47 @@
 window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
+   
+    
+	//	let list = document.querySelector("#mySelect");
+	//	for (let i = 0; i < countries.length; i++) {
+	//		let opt = document.createElement("option");
+	//		opt.innerHTML = countries[i];
+	//		list.appendChild(opt);
+	//	}
+	
+    //	let childs = list.childNodes;
+    //	list.addEventListener("change", function() {
+    //		for (let i = 0; i < childs.length; i++) {
+    //			if (childs[i].selected === true) {
+    //				switch (i) {
+    //					case 3:
+    //						alert("USA");   // code block
+    //						break;
+    //					case 4:
+    //						alert("France");    // code block
+    //						break;
+    //					case 5:
+    //						alert("Italy");     // code block
+    //						break;
+    //					case 6:
+    //						alert("Brazil");    // code block
+    //						break;
+    //					case 7:
+    //						alert("Colombia");  // code block
+    //						break;
+    //					case 8:
+    //						alert("Belize");    // code block
+    //						break;
+    //					case 9:
+    //						alert("Venezuela");     // code block
+    //						break;
+    //					default:
+    //						alert("Select your country");   // code block
+    //				}
+    //			}
+    //		}
+    //    });
+    
 
-  // your code here
-
-  
 };
--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,29 @@
 // Your code here
+let campo = document.getElementById("addToDo");
+console.log(campo);
+campo.addEventListener("keydown", event => {
+	// keypress = enter
+	if (event.key === "Enter") {
+		let lista = document.querySelector("ul");
+
+		let deberNuevo = document.createElement("li");
+		deberNuevo.innerHTML = `<span><i class="fa fa-trash"></i></span> ${campo.value}`;
+
+		let button = deberNuevo.firstChild.firstChild;
+		button.addEventListener("click", () => {
+			lista.removeChild(deberNuevo);
+		});
+
+		lista.appendChild(deberNuevo);
+
+		campo.value = "";
+	}
+});
+
+let list = document.querySelector("ul");
+let trash = document.querySelectorAll(".fa-trash");
+trash.forEach(item => {
+	item.addEventListener("click", () => {
+		list.removeChild(item.parentNode.parentNode);
+	});
+});
